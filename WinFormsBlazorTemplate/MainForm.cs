1524235c19158cf68cc18394a007bd4f223e9748
yuvalkubiyax <138358898+yuvalkubiyax@users.noreply.github.com>
2024-11-19 08:52:59 +0200

    template project created

---
+++ WinFormsBlazorTemplate/MainForm.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Components.WebView.WindowsForms;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WinFormsBlazorTemplate
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            var services = new ServiceCollection();
+            services.AddWindowsFormsBlazorWebView();
+            blazorWebView1.HostPage = "wwwroot\\index.html";
+            blazorWebView1.Services = services.BuildServiceProvider();
+            blazorWebView1.RootComponents.Add<Counter>("#app");
+        }
+    }
+}
